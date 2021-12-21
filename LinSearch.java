a861f0c5c024163ed79c4c3884f1713d299781d4
Hugo Jenkins <90857207+hugojenkins11@users.noreply.github.com>
2021-12-20 23:22:30 -0500

    Update LinSearch.java

--- LinSearch.java
+++ LinSearch.java
@@ -1,7 +1,10 @@
-// YAH:: Ameer Alnasser+ TurtleBoi, Hugo Jenkins + Boary
-// APCS pd6
-// Lab03
-//2021-12-20
+/*
+YAH:: Ameer Alnasser + TurtleBoi, Hugo Jenkins + Boary
+APCS pd6
+Lab03:: Search
+2021-12-20
+Time Spent: 6 hours
+*/
 
 /**
    class LinSearch
